2fda10cafa47e9d5f56103398ff247dd406130bd
imasi <imasi@NEVERGIVEUP>
2024-02-01 13:40:29 -0500

    Add project files.

---
+++ MyMovies/Controllers/MoviesController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+using MyMovies.Data;
+
+namespace MyMovies.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly MyMoviesContext _context;
+
+        public MoviesController(MyMoviesContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Movies
+        public async Task<IActionResult> Index(string searchString)
+        {
+            try
+            {
+                // Ensure the movie entity set is not null
+                if (_context.Movie == null)
+                {
+                    return Problem("Entity set 'MyMoviesContext.Movie' is null.");
+                }
+
+                // Retrieve all movies
+                var movies = from m in _context.Movie
+                             select m;
+
+                // Filter movies based on the search string
+                if (!String.IsNullOrEmpty(searchString))
+                {
+                    movies = movies.Where(s => s.Title.Contains(searchString));
+                }
+
+                // Return the filtered movies to the view
+                return View(await movies.ToListAsync());
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions, log, or return an error view as needed
+                return Problem($"An error occurred: {ex.Message}");
+            }
+        }
+
+
+        // GET: Movies/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Movies/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(movie);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movie);
+        }
+
+        // GET: Movies/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie.FindAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: Movies/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie)
+        {
+            if (id != movie.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(movie);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MovieExists(movie.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movie);
+        }
+
+        // GET: Movies/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        // POST: Movies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var movie = await _context.Movie.FindAsync(id);
+            if (movie != null)
+            {
+                _context.Movie.Remove(movie);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MovieExists(int id)
+        {
+            return _context.Movie.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ MyMovies/README.txt
@@ -0,0 +1,25 @@
+2024-01-17
+1412
+
+PartA: 
+firstly, i initialize the installing of ASP.net
+Part b:add controller
+then,i applied the controllers mvc and followed the steps and created the webpage.
+
+2024-01-25
+0144
+
+part-a
+firstly i 
+20240125184858_InitialCreate.cs
+
+2024-02-01
+0130
+
+firstly, i added or modified code according to the tutorial in order to make the search individually by movies name or initials.
+then, i again modified the code of moviescontroller.cs to pass the search totle as rout data.
+after that,i added the code to make the filter box.
+ Fixed this by specifying the request should be HTTP GET found in the Views/Movies/Index.cshtml file.
+
+
+
